646a2cb72c738936352714107da53f9c80861476
AdamManselll <adammansellhmb@gmail.com>
2020-09-04 14:07:46 -0700

    Return good fail for Venus life 4

--- __tests__/space-scripts.test.js
+++ __tests__/space-scripts.test.js
@@ -105,4 +105,11 @@ let human
     let exercise = human.exercise;
     expect(human.venusExpectCalc(sex, smoke, exercise)).toEqual(130);
   });
+  test("Should take Earth info of user who doesn't exercise and return their average life expectancy on Venus.", () => {
+    human = new Human('21', 'female', 'non-smoker', 'no');
+    let sex = human.sex;
+    let smoke = human.smoke;
+    let exercise = human.exercise;
+    expect(human.venusExpectCalc(sex, smoke, exercise)).toEqual(136);
+  });
 });
